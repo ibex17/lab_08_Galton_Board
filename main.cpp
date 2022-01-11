79b74c7bf1dd7b5d3ce43fb8a0c6dba5f3bfe183
ibex17 <richard.sierra@outlook.com>
2022-01-11 15:22:17 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+-------------------------------------------------------------------------------------------------
+File name       :   main.cpp
+Laboratory name :   Galton Board
+Author(s)       :   Richard SIERRA, Lucas LEISSING
+Creation date   :   11.01.22
+Description     :   <What does this program do ?>
+Remarks         :   <Is there a bug or something ?>
+Compiler        :   Apple clang version 13.0.0 (clang-1300.0.29.30)
+-------------------------------------------------------------------------------------------------
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    
+    
+    return EXIT_SUCCESS;
+}
